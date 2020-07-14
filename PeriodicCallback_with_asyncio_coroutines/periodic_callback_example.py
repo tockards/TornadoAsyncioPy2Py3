7dc168e0f6abaa469dddb2bb311296a9c7bc0a45
tockards <toufeeq.ockards+gitlab@gmail.com>
2020-07-14 09:58:13 +0200

    corrected TimeoutError import

--- PeriodicCallback_with_asyncio_coroutines/periodic_callback_example.py
+++ PeriodicCallback_with_asyncio_coroutines/periodic_callback_example.py
@@ -7,8 +7,8 @@ It works out of the box as the PeriodicCallback requires a Callable.
 from tornado.platform.asyncio import AsyncIOMainLoop
 from tornado import gen
 from tornado.ioloop import PeriodicCallback
-from asyncio import sleep
-
+import asyncio
+import tornado
 
 @gen.coroutine
 def hello_delay(wait=1):
@@ -18,13 +18,25 @@ def hello_delay(wait=1):
 
 async def hello_aiodelay(wait=1):
     print("Hello ...")
-    await sleep(wait)
+    await asyncio.sleep(wait)
     print("...World")
+
+
+async def hello_tornadoaiodelay(wait=1):
+    print("Hello ...")
+    await asyncio.sleep(wait)
+    print("...World")
+
+async def stop_at(runtime=10):
+    io_loop =
 
 if __name__ == '__main__':
     io_loop = AsyncIOMainLoop()
     pc = PeriodicCallback(hello_delay, 3*1000)
     aiopc = PeriodicCallback(hello_aiodelay, 4*1000)
+    pc = PeriodicCallback(hello_delay, 3 * 1000)
+
+
     aiopc.start()
     pc.start()
     io_loop.start()
